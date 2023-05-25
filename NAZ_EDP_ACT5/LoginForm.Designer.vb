d53e9e4d24acc7adcb7f40b802186d733081b397
jahhmwel <118279045+jahhmwel@users.noreply.github.com>
2023-05-26 03:58:22 +0800

    Add files via upload

---
+++ NAZ_EDP_ACT5/LoginForm.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.usernametxt = New System.Windows.Forms.TextBox()
+        Me.passwordtxt = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'usernametxt
+        '
+        Me.usernametxt.BackColor = System.Drawing.Color.LightBlue
+        Me.usernametxt.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.usernametxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.usernametxt.Location = New System.Drawing.Point(450, 162)
+        Me.usernametxt.Multiline = True
+        Me.usernametxt.Name = "usernametxt"
+        Me.usernametxt.Size = New System.Drawing.Size(279, 41)
+        Me.usernametxt.TabIndex = 0
+        '
+        'passwordtxt
+        '
+        Me.passwordtxt.BackColor = System.Drawing.Color.LightBlue
+        Me.passwordtxt.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.passwordtxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.passwordtxt.Location = New System.Drawing.Point(450, 256)
+        Me.passwordtxt.Multiline = True
+        Me.passwordtxt.Name = "passwordtxt"
+        Me.passwordtxt.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.passwordtxt.Size = New System.Drawing.Size(279, 41)
+        Me.passwordtxt.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.SteelBlue
+        Me.Button1.Font = New System.Drawing.Font("Microsoft YaHei", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.Button1.Location = New System.Drawing.Point(620, 353)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(109, 41)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Login"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.Button2.Font = New System.Drawing.Font("Microsoft YaHei", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.SystemColors.HighlightText
+        Me.Button2.Location = New System.Drawing.Point(450, 353)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(109, 41)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Sign up"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.RosyBrown
+        Me.Label1.Location = New System.Drawing.Point(370, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(418, 40)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "WELCOME TO FEAST HUB "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.RosyBrown
+        Me.Label2.Location = New System.Drawing.Point(445, 134)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(123, 25)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "USERNAME:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Microsoft YaHei", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.RosyBrown
+        Me.Label3.Location = New System.Drawing.Point(445, 228)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(126, 25)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "PASSWORD:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.passwordtxt)
+        Me.Controls.Add(Me.usernametxt)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.DoubleBuffered = True
+        Me.Name = "Form1"
+        Me.Text = "Feast Hub"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents usernametxt As TextBox
+    Friend WithEvents passwordtxt As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ NAZ_EDP_ACT5/usersForm.vb
@@ -0,0 +1,32 @@
+﻿Imports System.IO
+
+Public Class usersForm
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim openFileDialog As New OpenFileDialog()
+        openFileDialog.Filter = "CSV Files (*.csv)|*.csv"
+        openFileDialog.ShowDialog()
+
+        ' If the user selected a file, read it into a DataTable and bind it to the DataGridView.
+        If openFileDialog.FileName <> "" Then
+            Dim dt As New DataTable()
+            Dim fileReader As New StreamReader(openFileDialog.FileName)
+            Dim line As String = fileReader.ReadLine()
+            Dim columns As String() = line.Split(",")
+            For Each column As String In columns
+                dt.Columns.Add(column.Trim())
+            Next
+            While Not fileReader.EndOfStream
+                line = fileReader.ReadLine()
+                Dim fields As String() = line.Split(",")
+                dt.Rows.Add(fields)
+            End While
+            fileReader.Close()
+            DataGridView1.DataSource = dt
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        adminForm.Show()
+        Me.Hide()
+    End Sub
+End Class
